a7f0245dbc63932517d8ca4360de7607e9084e80
chrislasota <lasota371@gmail.com>
2024-12-02 14:51:23 -0700

    refactored, adding a function to validate each record

--- day_02/advent_02_1.py
+++ day_02/advent_02_1.py
@@ -2,6 +2,39 @@
 # Day 2 (Dec 2nd)
 # Problem 1
 
+def validate_record(levels):
+    # returns True if record is a safe one and False if not
+    how_many_levels = len(levels)
+    diff = levels[1] - levels[0]
+    if diff > 3 or diff < -3 or diff == 0:
+        # bad record, return immediately
+        return False
+    good_record = True
+    if diff > 0:
+        direction = "increasing"
+    if diff < 0:
+        direction = "decreasing"
+    for index in range(1, how_many_levels - 1):
+        diff = levels[index + 1] - levels[index]
+        if direction == "increasing" and diff <= 0:
+            # bad record
+            good_record = False
+            break
+        if direction == "increasing" and diff > 3:
+            # bad record
+            good_record = False
+            break
+        if direction == "decreasing" and diff >= 0:
+            # bad record
+            good_record = False
+            break
+        if direction == "decreasing" and diff < -3:
+            # bad record
+            good_record = False
+            break
+    return good_record
+
+
 def main():
     safe_report_count = 0
     input_file = open("input_day_02.txt", 'r')
@@ -9,36 +42,7 @@ def main():
     for line in lines:
         text_levels = line.split()
         levels = [int(x) for x in text_levels]
-        how_many_levels = len(levels)
-        diff = levels[1] - levels[0]
-        good_record = True
-        if diff > 3 or diff < -3 or diff == 0:
-            # bad record, skip to next record immediately
-            continue
-        if diff > 0:
-            direction = "increasing"
-        if diff < 0:
-            direction = "decreasing"
-        for index in range(1, how_many_levels - 1):
-            diff = levels[index + 1] - levels[index]
-            if direction == "increasing" and diff <= 0:
-                # bad record
-                good_record = False
-                break
-            if direction == "increasing" and diff > 3:
-                # bad record
-                good_record = False
-                break
-            if direction == "decreasing" and diff >= 0:
-                # bad record
-                good_record = False
-                break
-            if direction == "decreasing" and diff < -3:
-                # bad record
-                good_record = False
-                break
-
-        if good_record:
+        if validate_record(levels):
             safe_report_count += 1
 
     input_file.close()
