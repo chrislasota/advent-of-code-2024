2d7ad159961f124b5c47ab6f94c7eda2088e50e2
chrislasota <lasota371@gmail.com>
2024-12-03 04:57:55 -0700

    added code to solve part 1

---
+++ day_03/advent_03_1.py
@@ -0,0 +1,28 @@
+# Advent of Code 2024
+# Day 3
+# Part 1
+
+import re
+
+def main() -> int:
+    big_sum = 0
+    with open("input_day_03.txt", 'r') as input_file:
+        match_pattern = "mul[(][0-9]+,[0-9]+[)]"
+        for line in input_file:
+            mul_list = re.findall(match_pattern, line)
+            print(mul_list)
+            for operation in mul_list:
+                numbers_str = re.findall("[0-9]+,[0-9]+", operation)
+                operands = re.split(',', numbers_str[0])
+                print(operation)
+                print(numbers_str)
+                print(operands)
+                big_sum = big_sum + int(operands[0]) * int(operands[1])
+                print(big_sum)
+
+    input_file.close()
+    return big_sum
+
+
+if __name__ == "__main__":
+    print(f"The sum of all products is : {main()}")
