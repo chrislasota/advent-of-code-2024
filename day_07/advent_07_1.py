b0ae7d368c2c2609a60fb677d98ec9b1f0fed5e1
chrislasota <lasota371@gmail.com>
2024-12-07 18:32:52 -0700

    added code solution for part 1 of day 7

--- day_07/advent_07_1.py
+++ day_07/advent_07_1.py
@@ -4,15 +4,65 @@
 
 import re
 
+def create_operator_combo(value, width):
+    # Here we convert an integer into a string of '+' and '-' characters, as
+    # if it were a binary representation, but counting down, and we return it
+    # as a list.  We also prepend an extra "+" as the first operator.  All
+    # that truly matters is that there's a 1-to-1 map between values and
+    # unique lists of '+' and '*' characters
+    str = '+'
+    while value > 0:
+        if (value % 2) == 0:
+            str = str + '+'
+        else:
+            str = str + '*'
+        value = value // 2
+    while len(str) <= width:
+        str = str + '+'
+    return list(str)
+
+
+def the_math_works(operands, operators, result):
+    sum = 0
+    num_operands = len(operands)
+    for k in range(num_operands):
+        if operators[k] == '+':
+            sum = sum + operands[k]
+        else:
+            sum = sum * operands[k]
+    if sum == result:
+        return True
+    else:
+        return False
+
+
 def main() -> int:
     total_calibration_result = 0
     equations = []
-    with open("test.txt", 'r') as input_file:
+    with open("input_day_07.txt", 'r') as input_file:
         for line in input_file:
             line = line.strip()
             equations.append(line.split(': '))
 
-    print(equations)
+    # Now loop through each equation, and see if it can be made "correct"
+    # using some combination of the '+' and '-' operators
+    for eq in equations:
+        # convert target result to an integer
+        result = int(eq[0])
+        # grab the list of stringy operands
+        operands = re.findall("[0-9]+", eq[1])
+        # convert stringy operands to a list of integers
+        for op_index in range(len(operands)):
+            operands[op_index] = int(operands[op_index])
+        # figure out how many situations we might have to try
+        number_of_operators = len(operands) - 1
+        number_of_combos = 2**(number_of_operators)
+        # loop over each possible combination of operators
+        for combo in range(number_of_combos):
+            operators = create_operator_combo(combo, number_of_operators)
+            if the_math_works(operands, operators, result):
+                total_calibration_result += result
+                break
 
     return total_calibration_result
 
