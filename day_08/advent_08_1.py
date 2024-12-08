0a194b0b90f59a9e3afb6671cd690a642f628e86
chrislasota <lasota371@gmail.com>
2024-12-07 20:25:43 -0700

    created minimal code for the solutions for day 8

---
+++ day_08/advent_08_1.py
@@ -0,0 +1,18 @@
+# Advent of Code 2024
+# Day 8
+# Part 1
+
+# import re
+
+def main() -> int:
+    answer = 0
+#    with open("input_day_08.txt", 'r') as input_file:
+    with open("test.txt", 'r') as input_file:
+        for line in input_file:
+            line = line.strip()
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(f"The answer is : {main()}")
